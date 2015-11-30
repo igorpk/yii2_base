f66463f04ec107784f3b6d434010c752f9764f6c
Igor Kolodziejczyk <igorpk@pf-dev-008.office>
2015-11-30 17:06:02 +0200

    Initial Upload of files

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=pf_antifraud',
+    'username' => 'root',
+    'password' => 'linenoise0',
+    'charset' => 'utf8',
+];
